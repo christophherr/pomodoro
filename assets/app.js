f62fe7ef7b70c759eca489fa767d1dfb6f02f411
christophherr <info@christophherr.com>
2016-11-05 10:02:06 -0400

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+$('.add-minutes').click(function(){
+		 increaseTimerLength();
+	});
+
+$('.reduce-minutes').click(function(){
+		decreaseTimerLength();
+	});
+
+$('.start-button').click(function(){
+	clock.start();
+	});
+
+$('.reset-button').click(function(){
+	  clock.stop();
+  });
+
+var duration = 1500;
+
+var clock = $('.show-minutes').FlipClock(1500, {
+	autoStart: false,
+  countdown: true,
+	clockFace: 'MinuteCounter',
+  callbacks: {
+    stop: function() {
+      duration = 1500;
+      clock.setTime(1500)
+   },
+ },
+});
+
+function increaseTimerLength() {
+	var modifier = duration + 60;
+  clock.setTime(modifier);
+  duration += 60;
+}
+
+function decreaseTimerLength() {
+	var modifier = duration - 60;
+  if(modifier >= 0) {
+    clock.setTime(modifier);
+  } else {
+    return;
+  }
+  duration -= 60;
+}
+});
