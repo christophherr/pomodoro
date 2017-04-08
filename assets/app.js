5d5acf780108365868624933dfc248c85473d191
christophherr <info@christophherr.com>
2017-04-08 16:29:08 -0400

    formatting changes after running prettier

--- assets/app.js
+++ assets/app.js
@@ -1,47 +1,48 @@
 $( document ).ready(function() {
-$('.add-minutes').click(function(){
-		 increaseTimerLength();
-	});
 
-$('.reduce-minutes').click(function(){
-		decreaseTimerLength();
-	});
+$('.add-minutes').click(function() {
+    increaseTimerLength();
+});
 
-$('.start-button').click(function(){
-	clock.start();
-	});
+$('.reduce-minutes').click(function() {
+    decreaseTimerLength();
+});
 
-$('.reset-button').click(function(){
-	  clock.stop();
-  });
+$('.start-button').click(function() {
+    clock.start();
+});
+
+$('.reset-button').click(function() {
+    clock.stop();
+});
 
 var duration = 1500;
 
 var clock = $('.show-minutes').FlipClock(1500, {
-	autoStart: false,
-  countdown: true,
-	clockFace: 'MinuteCounter',
-  callbacks: {
-    stop: function() {
-      duration = 1500;
-      clock.setTime(1500)
-   },
- },
+    autoStart: false,
+    countdown: true,
+    clockFace: 'MinuteCounter',
+    callbacks: {
+        stop: function() {
+            duration = 1500;
+            clock.setTime(1500);
+        }
+    }
 });
 
 function increaseTimerLength() {
-	var modifier = duration + 60;
-  clock.setTime(modifier);
-  duration += 60;
+    var modifier = duration + 60;
+     clock.setTime(modifier);
+    duration += 60;
 }
 
 function decreaseTimerLength() {
-	var modifier = duration - 60;
-  if(modifier >= 0) {
-    clock.setTime(modifier);
-  } else {
-    return;
-  }
-  duration -= 60;
+    var modifier = duration - 60;
+     if (modifier >= 0) {
+        clock.setTime(modifier);
+     } else {
+        return;
+     }
+    duration -= 60;
 }
 });
