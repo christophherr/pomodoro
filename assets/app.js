84a294cbd33c53e8b70f80c919b7da56d0a06816
Christoph Herr <info@christophherr.com>
2017-12-08 20:27:39 -0500

    Update code style

--- assets/app.js
+++ assets/app.js
@@ -1,48 +1,50 @@
-$( document ).ready(function() {
+// @ts-check @ignore $
 
-$('.add-minutes').click(function() {
-    increaseTimerLength();
-});
+$(document).ready(() => {
+  let duration = 1500;
 
-$('.reduce-minutes').click(function() {
-    decreaseTimerLength();
-});
-
-$('.start-button').click(function() {
-    clock.start();
-});
-
-$('.reset-button').click(function() {
-    clock.stop();
-});
-
-var duration = 1500;
-
-var clock = $('.show-minutes').FlipClock(1500, {
+  const clock = $('.show-minutes').FlipClock(1500, {
     autoStart: false,
     countdown: true,
     clockFace: 'MinuteCounter',
     callbacks: {
-        stop: function() {
-            duration = 1500;
-            clock.setTime(1500);
-        }
+      stop() {
+        duration = 1500;
+        clock.setTime(1500);
+      }
     }
-});
+  });
 
-function increaseTimerLength() {
-    var modifier = duration + 60;
-     clock.setTime(modifier);
+  function increaseTimerLength() {
+    const modifier = duration + 60;
+    clock.setTime(modifier);
     duration += 60;
-}
+  }
 
-function decreaseTimerLength() {
-    var modifier = duration - 60;
-     if (modifier >= 0) {
-        clock.setTime(modifier);
-     } else {
-        return;
-     }
+  function decreaseTimerLength() {
+    const modifier = duration - 60;
+    if (modifier >= 0) {
+      clock.setTime(modifier);
+    } else {
+      return;
+    }
     duration -= 60;
-}
+  }
+
+
+  $('.add-minutes').click(() => {
+    increaseTimerLength();
+  });
+
+  $('.reduce-minutes').click(() => {
+    decreaseTimerLength();
+  });
+
+  $('.start-button').click(() => {
+    clock.start();
+  });
+
+  $('.reset-button').click(() => {
+    clock.stop();
+  });
 });
